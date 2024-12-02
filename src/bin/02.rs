edc90794f8f31357f07dbd3572307b6777875251
ClausNMadsen <claus.normann@hotmail.com>
2024-12-02 22:34:54 +0100

    Day 2: part 1 solved!

---
+++ src/bin/02.rs
@@ -0,0 +1,84 @@
+use nom::branch::alt;
+use nom::character::complete::{digit1, space1};
+use nom::combinator::{map_res, recognize};
+use nom::multi::many1;
+use nom::sequence::terminated;
+use nom::IResult;
+
+fn main() {
+    let text: String =
+        std::fs::read_to_string("data/02.txt").expect("Couldn't read file at hard-coded path!");
+    println!("Part 1:\n{}", part_1(&text));
+}
+
+fn parse_levels(line: &str) -> IResult<&str, Vec<i64>> {
+    many1(alt((
+        terminated(map_res(recognize(digit1), str::parse), space1),
+        map_res(recognize(digit1), str::parse),
+    )))(line)
+}
+
+fn part_1_criterion(line: &[i64]) -> bool {
+    (line.iter().is_sorted() || line.iter().rev().is_sorted())
+        && line.windows(2).all(|a| {
+            let diff = a[0].abs_diff(a[1]);
+            diff <= 3 && diff != 0
+        })
+}
+
+fn part_1(text: &str) -> usize {
+    text.lines()
+        .map(|l| {
+            let (_, parser_results) = parse_levels(l).unwrap();
+            parser_results
+        })
+        .filter(|l| part_1_criterion(l))
+        .count()
+}
+
+// fn part_2(text: &str) -> usize {
+//     let levels: Vec<Vec<i64>> = text
+//         .lines()
+//         .map(|l| {
+//             let (_, level_ints) = parse_levels(l).unwrap();
+//             level_ints
+//         })
+//         .collect();
+// }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    const INPUT: &str = "\
+7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9";
+
+    #[test]
+    fn parser_test() {
+        assert_eq!(
+            INPUT
+                .lines()
+                .map(parse_levels)
+                .map(|a| a.unwrap())
+                .map(|(_, a)| a)
+                .collect::<Vec<Vec<i64>>>(),
+            vec![
+                vec![7, 6, 4, 2, 1],
+                vec![1, 2, 7, 8, 9],
+                vec![9, 7, 6, 2, 1],
+                vec![1, 3, 2, 4, 5],
+                vec![8, 6, 4, 4, 1],
+                vec![1, 3, 6, 7, 9],
+            ]
+        );
+    }
+
+    #[test]
+    fn part_1_test() {
+        assert_eq!(part_1(INPUT), 2);
+    }
+}
