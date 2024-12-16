adcaa12a4dd4fb92a111e79539072eda35cd6d52
ClausNMadsen <claus.normann@hotmail.com>
2024-12-16 21:31:35 +0100

    day 9 part 2 solved ineffectively with dumb recursion

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -3,19 +3,37 @@ use nom::combinator::map_res;
 use nom::multi::many1;
 use nom::IResult;
 
-use std::{collections::VecDeque, ops::Div};
+use std::{collections::VecDeque, iter::repeat_n, ops::Div};
 
 fn main() {
     let text: String =
         std::fs::read_to_string("data/09.txt").expect("Couldn't read file at hard-coded path!");
     println!("Part 1:\n{}", part_1(&text));
-    // println!("Part 2:\n{}", part_2(&text));
+    println!("Part 2:\n{}", part_2(&text));
 }
 
 #[derive(Debug, PartialEq)]
 enum BlockType {
     File { index: u64 },
     Empty,
+}
+
+impl BlockType {
+    /// Returns `true` if the block type is [`File`].
+    ///
+    /// [`File`]: BlockType::File
+    #[must_use]
+    const fn is_file(&self) -> bool {
+        matches!(self, Self::File { .. })
+    }
+
+    /// Returns `true` if the block type is [`Empty`].
+    ///
+    /// [`Empty`]: BlockType::Empty
+    #[must_use]
+    const fn is_empty(&self) -> bool {
+        matches!(self, Self::Empty)
+    }
 }
 
 #[derive(Debug, PartialEq)]
@@ -111,6 +129,83 @@ fn part_1(input: &str) -> u64 {
         .iter()
         .enumerate()
         .map(|(n, e): (usize, &u64)| (n as u64) * e)
+        .sum()
+}
+
+fn try_move_file_to_space(disc: VecDeque<Block>) -> VecDeque<Block> {
+    for (n, file) in disc
+        .iter()
+        .enumerate()
+        .rev()
+        .filter(|(_n, b)| b.variant.is_file())
+    {
+        if let Some((m, _empty_block)) = disc
+            .iter()
+            .enumerate()
+            .find(|(m, empty)| *m < n && empty.variant.is_empty() && empty.count >= file.count)
+        {
+            let mut new_disc = disc;
+            let file_block: Block = new_disc.remove(n).expect("We just found this file!");
+            new_disc.insert(
+                n,
+                Block {
+                    variant: BlockType::Empty,
+                    count: file_block.count,
+                },
+            );
+            new_disc.insert(m, file_block);
+            new_disc
+                .get_mut(m + 1)
+                .expect("We just pushed an element to this location by an insert!")
+                .count -= new_disc.get(m).expect("We just pushed this value!").count;
+            return try_move_file_to_space(new_disc);
+        }
+    }
+    disc
+}
+
+#[derive(Debug, Clone, PartialEq)]
+enum Part2Index {
+    Index(u64),
+    Empty,
+}
+
+fn part_2_flatten_disc_for_hash(disc: &VecDeque<Block>) -> Vec<Part2Index> {
+    disc.iter()
+        .fold(Vec::new(), |mut acc, block| match block.variant {
+            BlockType::File { index } => {
+                acc.extend(repeat_n(
+                    Part2Index::Index(index),
+                    block
+                        .count
+                        .try_into()
+                        .expect("We are scanning a small compressed file."),
+                ));
+                acc
+            }
+            BlockType::Empty => {
+                acc.extend(repeat_n(
+                    Part2Index::Empty,
+                    block
+                        .count
+                        .try_into()
+                        .expect("We are scanning a small compressed file."),
+                ));
+                acc
+            }
+        })
+}
+
+fn part_2(input: &str) -> u64 {
+    let disc = first_parse_part_1(input);
+    let moved_disc = try_move_file_to_space(disc);
+    part_2_flatten_disc_for_hash(&moved_disc)
+        .iter()
+        .enumerate()
+        .filter_map(|(n, part_2_idx)| match part_2_idx {
+            Part2Index::Index(id) => Some(*id * (n as u64)),
+            Part2Index::Empty => None,
+        })
         .sum()
 }
 
@@ -218,5 +313,63 @@ mod test {
     #[test]
     fn day_9_part_1() {
         assert_eq!(part_1(INPUT), 1928);
+    }
+
+    #[test]
+    fn day_9_part_2_move_files_test() {
+        let disc = try_move_file_to_space(first_parse_part_1(INPUT));
+        let compact = part_2_flatten_disc_for_hash(&disc);
+        assert_eq!(
+            compact,
+            vec![
+                Part2Index::Index(0),
+                Part2Index::Index(0),
+                Part2Index::Index(9),
+                Part2Index::Index(9),
+                Part2Index::Index(2),
+                Part2Index::Index(1),
+                Part2Index::Index(1),
+                Part2Index::Index(1),
+                Part2Index::Index(7),
+                Part2Index::Index(7),
+                Part2Index::Index(7),
+                Part2Index::Empty,
+                Part2Index::Index(4),
+                Part2Index::Index(4),
+                Part2Index::Empty,
+                Part2Index::Index(3),
+                Part2Index::Index(3),
+                Part2Index::Index(3),
+                Part2Index::Empty,
+                Part2Index::Empty,
+                Part2Index::Empty,
+                Part2Index::Empty,
+                Part2Index::Index(5),
+                Part2Index::Index(5),
+                Part2Index::Index(5),
+                Part2Index::Index(5),
+                Part2Index::Empty,
+                Part2Index::Index(6),
+                Part2Index::Index(6),
+                Part2Index::Index(6),
+                Part2Index::Index(6),
+                Part2Index::Empty,
+                Part2Index::Empty,
+                Part2Index::Empty,
+                Part2Index::Empty,
+                Part2Index::Empty,
+                Part2Index::Index(8),
+                Part2Index::Index(8),
+                Part2Index::Index(8),
+                Part2Index::Index(8),
+                Part2Index::Empty,
+                Part2Index::Empty,
+            ]
+        );
+    }
+
+    #[test]
+    fn day_9_part_2() {
+        assert_eq!(part_2(INPUT), 2858);
     }
 }
