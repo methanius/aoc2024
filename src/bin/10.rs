68387134dd7ea2ae155a8982a9d441a8b3304e57
ClausNMadsen <claus.normann@hotmail.com>
2024-12-18 21:19:26 +0100

    Solved day 10 part 1

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -1,8 +1,35 @@
+use std::collections::{HashMap, HashSet};
+
 fn main() {
-    // let text: String =
-    //     std::fs::read_to_string("data/10.txt").expect("Couldn't read file at hard-coded path!");
-    // println!("Part 1:\n{}", part_1(&text));
+    let text: String =
+        std::fs::read_to_string("data/10.txt").expect("Couldn't read file at hard-coded path!");
+    println!("Part 1:\n{}", part_1(&text));
     // println!("Part 2:\n{}", part_2(&text));
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
+struct Position {
+    row: u64,
+    col: u64,
+}
+
+impl Position {
+    const fn new(row: u64, col: u64) -> Self {
+        Self { row, col }
+    }
+
+    fn direct_neighbours(&self) -> Vec<Self> {
+        let min_row = if self.row == 0 { 0 } else { self.row - 1 };
+        let min_col = if self.col == 0 { 0 } else { self.col - 1 };
+        let mut res: Vec<Self> = Vec::new();
+        for row in min_row..=self.row + 1 {
+            res.push(Self::new(row, self.col));
+        }
+        for col in min_col..=self.col + 1 {
+            res.push(Self::new(self.row, col));
+        }
+        res
+    }
 }
 
 #[derive(Debug, PartialEq)]
@@ -11,11 +38,24 @@ struct Grid {
 }
 
 impl Grid {
-    // fn get(&self, row: u64, col: u64) -> Option<u64> {
-    //     match (row.try_into::<usize>(), col.try_into::<usize>()) {
-    //         (Ok(_), Ok(_)) => todo!(),
-    //         (Ok(_), Err(_))  | (Err(_), Ok(_))  | (Err(_), Err(_)) => None,
-    //     }
+    fn get(&self, position: &Position) -> Option<&u64> {
+        self.data
+            .get(usize::try_from(position.row).expect("The Advent of Code grid is not that big!"))
+            .map(|c| {
+                c.get(
+                    usize::try_from(position.col).expect("The AoC puzzle grid is not that large!"),
+                )
+            })?
+    }
+
+    fn to_indexed_iterator(&self) -> impl Iterator<Item = (Position, &u64)> + use<'_> {
+        self.data.iter().enumerate().flat_map(|(row, row_vector)| {
+            row_vector
+                .iter()
+                .enumerate()
+                .map(move |(col, digit)| (Position::new(row as u64, col as u64), digit))
+        })
+    }
 }
 
 fn parse_grid(value: &str) -> Grid {
@@ -34,6 +74,41 @@ fn parse_grid(value: &str) -> Grid {
             })
             .collect(),
     }
+}
+
+fn walk_to_trail_end<'a>(grid: &'a Grid, start: &'a Position) -> HashSet<Position> {
+    let mut current_iteration_positions: Vec<Position> = Vec::from([*start]);
+    for target_at_step in 1..=9 {
+        let mut next_iteration_positions = Vec::new();
+        for position in &current_iteration_positions {
+            for neighbour in position.direct_neighbours() {
+                if grid.get(&neighbour) == Some(&target_at_step) {
+                    next_iteration_positions.push(neighbour);
+                }
+            }
+        }
+        current_iteration_positions = next_iteration_positions;
+    }
+    let mut res = HashSet::new();
+    res.extend(current_iteration_positions);
+    res
+}
+
+fn part_1(input: &str) -> u64 {
+    let grid = parse_grid(input);
+    grid.to_indexed_iterator()
+        .filter(|(_pos, height)| **height == 0)
+        .fold(
+            HashMap::new(),
+            |mut acc: HashMap<Position, HashSet<Position>>, (pos, _height): (Position, &u64)| {
+                acc.entry(pos)
+                    .or_insert_with(|| walk_to_trail_end(&grid, &pos));
+                acc
+            },
+        )
+        .values()
+        .map(|heads: &HashSet<Position>| heads.len() as u64)
+        .sum()
 }
 
 #[cfg(test)]
@@ -64,9 +139,22 @@ mod test {
                     vec![0, 1, 3, 2, 9, 8, 0, 1,],
                     vec![1, 0, 4, 5, 6, 7, 3, 2,],
                 ],
-                rows: 8,
-                cols: 8,
             }
         );
+    }
+
+    #[test]
+    fn day_10_test_walk_to_trail_end() {
+        let grid = parse_grid(INPUT);
+        assert_eq!(walk_to_trail_end(&grid, &Position::new(0, 2)).len(), 5);
+        assert_eq!(walk_to_trail_end(&grid, &Position::new(0, 4)).len(), 6);
+        assert_eq!(walk_to_trail_end(&grid, &Position::new(2, 4)).len(), 5);
+        assert_eq!(walk_to_trail_end(&grid, &Position::new(4, 6)).len(), 3);
+        assert_eq!(walk_to_trail_end(&grid, &Position::new(5, 2)).len(), 1);
+    }
+
+    #[test]
+    fn day_10_test_part_1() {
+        assert_eq!(part_1(INPUT), 36);
     }
 }
